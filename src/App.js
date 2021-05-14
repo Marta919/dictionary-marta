5962500d76ca0a6c14a3e65be065b3b9d97e1150
Marta919 <72815759+Marta919@users.noreply.github.com>
2021-05-14 11:28:21 +0200

    useState to be able to reproduce the word I am searching for in the pop-up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import './App.css';
+import Dictionary from "./Dictionary";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App(){
   return (
-    <p> Ciao </p>
+    <div className="container">
+    <h1> Dictionary for Everyone </h1>
+    <Dictionary />
+    <h2> Which words do you want to put in your mind? </h2>
+    <footer className="open"> Coded by <a href="https://github.com/Marta919"> Marta D'Orazio </a></footer>
+    </div>
   );
 }
 
